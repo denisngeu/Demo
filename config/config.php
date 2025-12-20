7fa2cf4691345089d6e674890c14e6bd20c81ecf
denisngeu <54903642+denisngeu@users.noreply.github.com>
2025-12-20 04:48:52 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,14 +1,10 @@
 <?php
-// Configuration de la base de données
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'ecommerce_db');
-
-// Configuration du site
-define('SITE_NAME', 'E-Commerce Velo');
-define('SITE_URL', 'http://localhost/e-com');
-
-// Configuration de session
 session_start();
+
+$conn = new PDO(
+ "mysql:host=localhost;dbname=ecommerce_demo",
+ "root",
+ "",
+ [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+);
 ?>
